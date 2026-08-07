a0d2bc655d2993e3b5407ceaf5407fe69eaadb13
Leonardo <leonardo.hnascimento90@gmail.com>
2026-08-06 22:06:28 -0300

    feat: adiciona seção de serviços

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { About } from "@/components/home/About";
 import { Hero } from "@/components/home/Hero";
+import { Services } from "@/components/home/Services";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
+      <Services />
     </>
   );
 }
