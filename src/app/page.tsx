4447600b0c6d384a91cc50199ef292644e3335c1
Leonardo <leonardo.hnascimento90@gmail.com>
2026-08-06 22:00:18 -0300

    feat: adiciona seção institucional sobre a EcoAdvanced

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { About } from "@/components/home/About";
 import { Hero } from "@/components/home/Hero";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <About />
+    </>
+  );
 }
