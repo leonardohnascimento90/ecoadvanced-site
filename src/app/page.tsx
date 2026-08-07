b5567ac182181364114b06b90fab62d7003702ed
Leonardo <leonardo.hnascimento90@gmail.com>
2026-08-06 22:11:14 -0300

    feat: adiciona seção de diferenciais

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { About } from "@/components/home/About";
 import { Hero } from "@/components/home/Hero";
 import { Services } from "@/components/home/Services";
+import { WhyChoose } from "@/components/home/WhyChoose";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <About />
       <Services />
+      <WhyChoose />
     </>
   );
 }
